a6e80dd791517b3a39f52637267014b3b1784813
Jon Linnell <linnell.jon@gmail.com>
2019-03-13 21:32:09 +0000

    Outline client

---
+++ client/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Test = () => (
+  <div>
+    <h2>Test</h2>
+  </div>
+)
+
+render(<Test />, document.getElementById('root'));
